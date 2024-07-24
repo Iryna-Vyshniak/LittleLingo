424a63e8bee77d6f5927c6266aa49365e84ea2b2
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-25 02:00:57 +0300

    feat: Added AppTitle component

---
+++ src/components/common/AppTitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AppTitle: React.FC<{ title: string; subtitle: string }> = ({ title, subtitle }) => {
+  return (
+    <div className='absolute top-0 left-1/2 transform -translate-x-1/2 p-4 bg-gradient-custom bg-clip-text text-transparent drop-shadow-[2px_5px_2px_rgba(15,41,1,1)]'>
+      <p className='text-6xl font-bold'>
+        {title}
+        <span>{subtitle}</span>
+      </p>
+    </div>
+  );
+};
+
+export default AppTitle;
