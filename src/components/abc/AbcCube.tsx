9965191bfd4634447882030507c808c836ce0b6d
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-28 18:17:03 +0300

    feat: Create AbcCube component with cube effect swiper for alphabet slides
    - Developed `AbcCube` component using Swiper and React to display alphabet slides with a 3D cube effect.
    - Utilized `Swiper` and `SwiperSlide` components from 'swiper/react', integrating the `EffectCube` module for the cube transition effect.
    - Implemented `handlePlayAudio` function to play and pause audio associated with each slide, and to navigate to the next slide on click.
    - Used `useRef` to manage references to the audio element and swiper instance, and `useState` to track audio playback state.
    - Rendered each slide with the letter image and an audio element, repeating the same slide content for the cube effect.
    - Styled the component with custom CSS in `AbcCube.css`, defining dimensions, padding, cursor styles, and background properties for the swiper and slides.
    - Ensured responsive and interactive design for presenting alphabet letters with audio playback in a visually engaging manner.
    - Exported `AbcCube` for integration into the application, enhancing the user experience with a fun and interactive learning tool.

---
+++ src/components/abc/AbcCube.tsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { EffectCube } from 'swiper/modules';
+
+import { LetterProps } from '../../shared/types';
+
+import './AbcCube.css';
+
+const AbcCube: React.FC<LetterProps> = ({ letter }) => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const swiperRef = useRef<any | null>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const handlePlayAudio = () => {
+    if (isPlaying) {
+      audioRef.current!.pause();
+    } else {
+      audioRef.current!.play();
+    }
+    setIsPlaying(!isPlaying);
+    // Move to the next slide
+    if (swiperRef.current) {
+      swiperRef.current!.swiper.slideNext();
+    }
+  };
+
+  // Create an array with multiple instances of the same slide
+  const slides = Array.from({ length: 12 }).map((_, index) => (
+    <SwiperSlide key={`${letter._id}-${index}`} className='abc-slide p-6'>
+      <div className='flex items-center justify-center object-contain'>
+        <img src={letter.image} alt={letter.label} className='h-16' />
+        <audio ref={audioRef} className='w-full h-full'>
+          <source src={letter.sound} type='audio/mp3' />
+        </audio>
+      </div>
+    </SwiperSlide>
+  ));
+
+  return (
+    <li className='w-40 h-40 flex items-center justify-center'>
+      <Swiper
+        effect={'cube'}
+        loop={true}
+        modules={[EffectCube]}
+        onClick={handlePlayAudio}
+        ref={swiperRef}
+      >
+        {slides}
+      </Swiper>
+    </li>
+  );
+};
+
+export default AbcCube;
