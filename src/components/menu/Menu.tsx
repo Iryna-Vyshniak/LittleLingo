c7368049ed0d9282766ce877fa696d8fa1a16e13
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-28 17:54:38 +0300

    feat: Implemented Menu component with navigation and icons
    - Created `Menu` component using Ionic's `IonMenu`, `IonHeader`, `IonToolbar`, `IonTitle`, `IonContent`, `IonMenuToggle`, and `IonItem` components.
    - Utilized `menuPaths` data to dynamically generate menu items, including paths, names, and icons for navigation.
    - Each menu item is wrapped in `IonMenuToggle` to control the menu's toggle behavior, with `IonItem` used for navigation links and `IonIcon` for displaying corresponding icons.
    - Set `routerLink` on `IonItem` to enable navigation to different routes within the app.
    - Applied `fullscreen` class to `IonContent` to ensure full-screen rendering of the menu.
    - Exported the `Menu` component for integration into the application layout, providing a consistent and interactive navigation experience.

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,28 @@
+import { IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle,  IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+import { menuPaths } from '../../shared/data';
+
+const Menu: React.FC = () => {
+  return (
+    <IonMenu contentId='main'>
+      <IonHeader>
+        <IonToolbar color='secondary'>
+          <IonTitle>Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className='menu'>
+        {menuPaths.map(({ id, name, url, icon }) => (
+          <IonMenuToggle key={id} autoHide={false}>
+            <IonItem detail={true} routerLink={url}>
+              <IonIcon slot='start' icon={icon} />
+              {name}
+            </IonItem>
+          </IonMenuToggle>
+        ))}
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Menu;
