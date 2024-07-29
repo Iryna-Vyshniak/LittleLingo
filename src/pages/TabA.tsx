e099e15cd0c033857820f52fbdb8c38bd105f30d
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-29 13:57:43 +0300

    feat: Add navigation to ABC training page in TabA and TabB
    - Imported and used `useIonRouter` hook to access the router history within `TabA.tsx` and `TabB.tsx`.
    - Created `goToABC` function to handle navigation to the ABC training page (`/app/list/stagea/abc-train`). The function uses `history.push()` to navigate and prevents the default button action.
    - Added an `IonButton` component with an `onClick` handler set to `goToABC`, providing a user interface element for navigating to the ABC training page.
    - These additions enable seamless user transitions to the ABC training content from different tabs in the application, enhancing navigation and user experience.

--- src/pages/TabA.tsx
+++ src/pages/TabA.tsx
@@ -7,21 +7,32 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonRouter,
 } from '@ionic/react';
-import React from 'react';
+import React, { MouseEvent } from 'react';
 
 const TabA: React.FC = () => {
+  const history = useIonRouter();
+
+  const goToABC = (e: MouseEvent<HTMLIonButtonElement>) => {
+    e.preventDefault();
+    history.push('/app/list/stagea/abc-train');
+  };
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar color={'success'}>
+        <IonToolbar color='secondary'>
           <IonButtons slot='start'>
             <IonMenuButton />
           </IonButtons>
           <IonTitle>STAGE A</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>STAGE A</IonContent>
+      <IonContent className='ion-padding'>
+        <h1>STAGE A </h1>
+        <IonButton onClick={goToABC}>Go to ABC</IonButton>
+      </IonContent>
     </IonPage>
   );
 };
--- src/pages/TabB.tsx
+++ src/pages/TabB.tsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
@@ -6,21 +7,32 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonRouter,
 } from '@ionic/react';
-import React from 'react';
+import React, { MouseEvent } from 'react';
 
 const TabB: React.FC = () => {
+  const history = useIonRouter();
+
+  const goToABC = (e: MouseEvent<HTMLIonButtonElement>) => {
+    e.preventDefault();
+    history.push('/app/list/stagea/abc-train');
+  };
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar color={'success'}>
+        <IonToolbar color='secondary'>
           <IonButtons slot='start'>
             <IonMenuButton />
           </IonButtons>
           <IonTitle>STAGE B</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>STAGE B</IonContent>
+      <IonContent className='ion-padding'>
+        <h1>STAGE B </h1>
+        <IonButton onClick={goToABC}>Go to ABC</IonButton>
+      </IonContent>
     </IonPage>
   );
 };
