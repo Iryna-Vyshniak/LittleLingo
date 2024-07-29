c670746dd4a2a641f81425328f8f4f51a32c4a76
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-29 14:12:12 +0300

    refactor: Wrap ListPage content in IonPage for improved layout handling
    - Updated `ListPage` component to include an `IonPage` wrapper around the content.
    - This change enhances the layout management and styling consistency across the app, especially in ensuring proper page transitions and handling of Ionic's internal features.
    - The rest of the structure, including `IonTabs`, `IonRouterOutlet`, `IonTabBar`, and routes for `TabA`, `TabB`, and `AbcTrainPage`, remains unchanged, maintaining the existing navigation functionality.
    - The inclusion of `IonPage` also ensures that the component complies with best practices in Ionic app structure, providing a consistent base layout.

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,4 +1,4 @@
-import { IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
+import { IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import React from 'react';
 import { Route, Redirect } from 'react-router';
 
@@ -11,24 +11,26 @@ import AbcTrainPage from './AbcTrainPage';
 
 const ListPage: React.FC = () => {
   return (
-    <IonTabs>
-      <IonRouterOutlet>
-        <Route path='/app/list/stagea' exact component={TabA} />
-        <Route path='/app/list/stagea/abc-train' exact component={AbcTrainPage} />
-        <Route path='/app/list/stageb' exact component={TabB} />
-        <Route path='/app/list/stageb/abc-train' exact component={AbcTrainPage} />
-        <Redirect from='/app/list' to='/app/list/stagea' exact />
-      </IonRouterOutlet>
+    <IonPage>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path='/app/list/stagea' exact component={TabA} />
+          <Route path='/app/list/stagea/abc-train' exact component={AbcTrainPage} />
+          <Route path='/app/list/stageb' exact component={TabB} />
+          <Route path='/app/list/stageb/abc-train' exact component={AbcTrainPage} />
+          <Redirect from='/app/list' to='/app/list/stagea' exact />
+        </IonRouterOutlet>
 
-      <IonTabBar slot='bottom' color='secondary' mode='md'>
-        <IonTabButton tab='stagea' href='/app/list/stagea'>
-          <img src={StageA} alt='book stage' className='w-12 h-16' />
-        </IonTabButton>
-        <IonTabButton tab='stageb' href='/app/list/stageb'>
-          <img src={StageB} alt='book stage' className='w-12 h-16' />
-        </IonTabButton>
-      </IonTabBar>
-    </IonTabs>
+        <IonTabBar slot='bottom' color='secondary' mode='md'>
+          <IonTabButton tab='stagea' href='/app/list/stagea'>
+            <img src={StageA} alt='book stage' className='w-12 h-16' />
+          </IonTabButton>
+          <IonTabButton tab='stageb' href='/app/list/stageb'>
+            <img src={StageB} alt='book stage' className='w-12 h-16' />
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonPage>
   );
 };
 
