7a0792c5f4e19527aae9fffeab1f5b88f91c864c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-30 15:32:45 +0300

    feat: Add UIContext and UIProvider for managing UI state
    - Created `UIContext` using React's `createContext` to manage UI-related state across the application.
    - Implemented `useUIContext` hook to provide convenient access to the `UIContext`, with a safety check to ensure it's used within a `UIProvider`.
    - Developed `UIProvider` component to manage and provide reactive UI state, specifically `showTabs` and its setter `setShowTabs`.
      - The `showTabs` state controls the visibility of UI elements like tabs, allowing for dynamic UI adjustments.
      - The provider wraps the application and passes down the state and setter through context, enabling shared state management across components.
    - Exported `UIContext`, `useUIContext`, and `UIProvider` for integration into the application's component tree, enhancing the UI's responsiveness to state changes.

---
+++ src/shared/context/tab-context.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode, useContext, useState } from 'react';
+
+// create the context
+export const UIContext = React.createContext<any>(undefined);
+
+export const useUIContext = () => {
+  const context = useContext(UIContext);
+  if (context === undefined) {
+    throw new Error('useUIContext must be used within a UIProvider');
+  }
+  return context;
+};
+
+// create the context provider, we are using use state to ensure that
+// we get reactive values from the context...
+
+export const UIProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  // the reactive values
+  const [showTabs, setShowTabs] = useState<boolean>(true);
+
+  // the store object
+  const state = {
+    showTabs,
+    setShowTabs,
+  };
+
+  // wrap the application in the provider with the initialized context
+  return <UIContext.Provider value={state}>{children}</UIContext.Provider>;
+};
+
+export default UIContext;
