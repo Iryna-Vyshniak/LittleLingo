272290a8b8dde0115ed75dd8a61d9ddc983b96fd
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-30 15:33:03 +0300

    chore: Wrap App component with UIProvider for global UI state management
    - Added `UIProvider` around the `App` component in the `React.StrictMode` block.
    - This setup ensures that the global UI state, managed by `UIProvider`, is accessible throughout the entire application.
    - Enables consistent UI state management, such as controlling the visibility of UI elements like tabs, across all components.
    - This integration enhances the maintainability and scalability of the application by centralizing UI state logic.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { UIProvider } from './shared/context/tab-context';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <UIProvider>
+      <App />
+    </UIProvider>
   </React.StrictMode>
 );
