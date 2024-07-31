487889bd586b309e54dd711f434469d5f696cd27
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-07-31 23:43:04 +0300

    feat: Define TypeScript interfaces for game components
    - Added `ColorCard` interface with fields for id, name, image URL, match status, and sound.
    - Created `GameBoardCardProps` interface to handle properties for game board cards, including functions for handling card actions and determining styles dynamically.
    - Defined `GameInfoProps` interface to manage game information such as score and optional timer.
    - Introduced `ScoreCardProps` interface for displaying score details with a medal, title, and value.
    
    These interfaces are designed to strengthen type safety and ensure consistency across the game's UI components.

--- src/shared/types/index.tsx
+++ src/shared/types/index.tsx
@@ -8,3 +8,30 @@ export interface Letter {
 export interface LetterProps {
   letter: { _id: string; label: string; image: string; sound: string };
 }
+
+export interface ColorCard {
+  id: string;
+  name: string;
+  img: string;
+  matched: boolean;
+  sound: string;
+}
+
+export interface GameBoardCardProps {
+  card: ColorCard;
+  handleCard: (card: ColorCard) => void;
+  getRandomRadius: () => string;
+  flipped: boolean;
+  stopFlip: boolean;
+}
+
+export interface GameInfoProps {
+  score: string | number;
+  timer?: string;
+}
+
+export interface ScoreCardProps {
+  medal: string;
+  title: string;
+  value: string | number;
+}
