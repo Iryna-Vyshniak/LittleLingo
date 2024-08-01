8e529b43ac0c7eb59d01c92db5f2ddc91f0f41a2
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-01 03:46:16 +0300

    feat: Add GameBoardCard component for interactive game board display
    - Introduced `GameBoardCard` component using `IonCard` and `IonCardContent` from Ionic React for visual representation of game cards.
    - Incorporated dynamic border-radius styling using `getRandomRadius` function to enhance visual diversity.
    - Enabled card flip interaction, controlled by `flipped` and `stopFlip` props, to handle user game interactions:
      - Cards display the front face with an image when flipped.
      - The back face is interactive, allowing card handling via `handleCard` function unless flipping is disabled.
    - Utilized `IonImg` for optimized image rendering with specific alt text for better accessibility and SEO.
    
    This component enriches the game's interface by providing visually appealing and interactive cards that respond to user actions.

---
+++ src/components/game/GameBoardCard.tsx
@@ -0,0 +1,27 @@
+import { IonCard, IonCardContent, IonImg } from '@ionic/react';
+import React from 'react';
+
+import { GameBoardCardProps } from '../../shared/types';
+
+const GameBoardCard: React.FC<GameBoardCardProps> = ({
+  card,
+  handleCard,
+  getRandomRadius,
+  stopFlip,
+  flipped,
+}) => {
+  return (
+    <IonCard
+      key={card.id}
+      className='color-card w-full h-full m-0 aspect-square'
+      style={{ borderRadius: getRandomRadius() }}
+    >
+      <IonCardContent className={flipped ? 'flip' : ''}>
+        <IonImg src={card.img} alt={`${card.name} bottle`} className='face' />
+        <div className='back' onClick={() => !stopFlip && handleCard(card)}></div>
+      </IonCardContent>
+    </IonCard>
+  );
+};
+
+export default GameBoardCard;
