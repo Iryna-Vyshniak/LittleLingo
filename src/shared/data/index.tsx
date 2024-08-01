89fad6e0fdce5916a678c9104f2d807fdee767a5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-01 03:38:12 +0300

    feat: Populate colorsData array with color card details for game
    - Added `colorsData` array to `data.ts` containing structured data for color cards used in the game.
    - Each entry includes `id`, `name`, `img`, `matched`, and `sound` fields:
      - `id`: Unique identifier for each card.
      - `name`: Color name of the card.
      - `img`: Image component representing the color.
      - `matched`: Boolean to track if the card has been matched in the game.
      - `sound`: URL to the sound file associated with each color.
    - Ensured a diverse range of colors including green, yellow, orange, red, purple, pink, blue, navy, gray, and black.
    - Repeated some colors to facilitate matching mechanics in the game.
    
    This update aims to enhance the interactive elements of the game by providing visual and auditory cues associated with each color card.

--- src/shared/data/index.tsx
+++ src/shared/data/index.tsx
@@ -1,8 +1,22 @@
+import { homeOutline, settingsOutline } from 'ionicons/icons';
+
+// INTRO
 import Slide1Image from '../../assets/images/intro/intro.jpg';
 import Slide2Image from '../../assets/images/intro/intro-abc.jpg';
 import Slide3Image from '../../assets/images/intro/intro-colors.jpg';
 import Slide4Image from '../../assets/images/intro/intro-animals.jpg';
-import { homeOutline, settingsOutline } from 'ionicons/icons';
+
+// COLORS
+import Green from '../../assets/images/colors/bottles/green-potion.png';
+import Yellow from '../../assets/images/colors/bottles/yellow-potion.png';
+import Orange from '../../assets/images/colors/bottles/orange-potion.png';
+import Red from '../../assets/images/colors/bottles/red-potion.png';
+import Purple from '../../assets/images/colors/bottles/purple-potion.png';
+import Pink from '../../assets/images/colors/bottles/pink-potion.png';
+import Blue from '../../assets/images/colors/bottles/blue-potion.png';
+import Navy from '../../assets/images/colors/bottles/navy-potion.png';
+import Gray from '../../assets/images/colors/bottles/gray-potion.png';
+import Black from '../../assets/images/colors/bottles/black-potion.png';
 
 export const introImages = [
   {
@@ -39,4 +53,149 @@ export const introImages = [
 export const menuPaths = [
   { id: '2g45fqy77', name: 'Home', url: '/app/list', icon: homeOutline },
   { id: '2g1erqy78', name: 'Settings', url: '/app/settings', icon: settingsOutline },
+];
+
+export const colorsData = [
+  {
+    id: '13dss0',
+    name: 'green',
+    img: Green,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/green.mp3',
+  },
+  {
+    id: '13dss1',
+    name: 'yellow',
+    img: Yellow,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/yellow.mp3',
+  },
+  {
+    id: '13dss2',
+    name: 'orange',
+    img: Orange,
+    matched: false,
+    sound:
+      'https://archive.org/download/little-girl-says-orange-188230/little-girl-says-orange-188230.mp3',
+  },
+  {
+    id: '13dss3',
+    name: 'red',
+    img: Red,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/red.mp3',
+  },
+  {
+    id: '13dss4',
+    name: 'purple',
+    img: Purple,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/purple.mp3',
+  },
+  {
+    id: '13dss5',
+    name: 'pink',
+    img: Pink,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/pink.mp3',
+  },
+  {
+    id: '13dss6',
+    name: 'blue',
+    img: Blue,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/blue.mp3',
+  },
+  {
+    id: '13dss7',
+    name: 'navy',
+    img: Navy,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/navy.mp3',
+  },
+  {
+    id: '13dss8',
+    name: 'gray',
+    img: Gray,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/gray.mp3',
+  },
+  {
+    id: '13dss9',
+    name: 'black',
+    img: Black,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/black.mp3',
+  },
+  {
+    id: '13dss10',
+    name: 'green',
+    img: Green,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/green.mp3',
+  },
+  {
+    id: '13dss11',
+    name: 'yellow',
+    img: Yellow,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/yellow.mp3',
+  },
+  {
+    id: '13dss12',
+    name: 'orange',
+    img: Orange,
+    matched: false,
+    sound:
+      'https://archive.org/download/little-girl-says-orange-188230/little-girl-says-orange-188230.mp3',
+  },
+  {
+    id: '13dss13',
+    name: 'red',
+    img: Red,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/red.mp3',
+  },
+  {
+    id: '13dss14',
+    name: 'purple',
+    img: Purple,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/purple.mp3',
+  },
+  {
+    id: '13dss15',
+    name: 'pink',
+    img: Pink,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/pink.mp3',
+  },
+  {
+    id: '13dss16',
+    name: 'blue',
+    img: Blue,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/blue.mp3',
+  },
+  {
+    id: '13dss17',
+    name: 'navy',
+    img: Navy,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/navy.mp3',
+  },
+  {
+    id: '13dss18',
+    name: 'gray',
+    img: Gray,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/gray.mp3',
+  },
+  {
+    id: '13dss19',
+    name: 'black',
+    img: Black,
+    matched: false,
+    sound: 'https://archive.org/download/turquoise_202407/black.mp3',
+  },
 ];
