8fd9efce32e7102887db0b5c172ff45ffb8b4b32
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-01 03:37:13 +0300

    feat: Add game configuration constants to enhance control and customization
    - Added `RADIUS` array with various percentage values for dynamic border-radius styles in game components.
    - Defined initial game settings with constants:
      - `INITIAL_TIMER` set to 60 seconds for game duration.
      - `INITIAL_MOVE` initialized at 0 to track the number of moves.
      - `INITIAL_SCORE` set to 0 as the starting score.
      - `SUCCESS_SCORE` set at 9, indicating points awarded for successful actions.
      - `FAILURE_SCORE` set at 5, indicating points deducted for failed actions.
    
    These constants will standardize game settings and allow for easier adjustments and testing.

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,1 +1,10 @@
 export const API = import.meta.env.VITE_API_KEY;
+
+export const RADIUS = ['50%', '54% 46% 55% 45% / 48% 54% 46% 52%', '54% 46% 55% 45% / 48% 60% 40% 52%', '54% 46% 55% 45% / 42% 60% 40% 58%', '50%', '62% 38% 55% 45% / 42% 60% 40% 58%', '50%', '62% 38% 55% 45% / 42% 50% 50% 58%', '50%', '51% 49% 55% 45% / 42% 50% 50% 58%', '50%'];
+
+export const INITIAL_TIMER = 60
+export const INITIAL_MOVE = 0
+export const INITIAL_SCORE = 0
+export const SUCCESS_SCORE = 9
+export const FAILURE_SCORE = 5
+
