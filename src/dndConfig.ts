e6ed24370ab425b84449074952bf409e4c88aa57
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-03 02:34:45 +0300

    feat: Configure drag-and-drop backends for multi-environment support
    - Created `dndConfig.ts` to define configurations for `react-dnd` drag-and-drop functionalities.
    - Set up `HTML5toTouch` configuration object to seamlessly switch between HTML5 and touch backends:
      - Configured `HTML5Backend` with `MouseTransition` to handle drag-and-drop operations on devices using a mouse.
      - Set up `TouchBackend` with `TouchTransition` and enabled mouse events, enhancing responsiveness on touch devices while still supporting mouse interactions.
      - Enabled a preview feature on the touch backend for better visual feedback during drag operations.
    
    This configuration ensures that our application's drag-and-drop functionality adapts to different user environments, providing smooth and intuitive interactions across all devices.

---
+++ src/dndConfig.ts
@@ -0,0 +1,20 @@
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { MouseTransition, TouchTransition } from "react-dnd-multi-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
+
+export const HTML5toTouch = {
+    backends: [
+        {
+            id: 'html5',
+            backend: HTML5Backend,
+            transition: MouseTransition,
+        },
+        {
+            id: 'touch',
+            backend: TouchBackend,
+            options: { enableMouseEvents: true },
+            preview: true,
+            transition: TouchTransition,
+        },
+    ],
+}
