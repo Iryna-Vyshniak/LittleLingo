4fe0a7f74c3ea59321085a65d4d7ab8f2721fd00
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-03 03:15:56 +0300

    feat: Implement ColorCardGame component with drag-and-drop functionality
    - Created `ColorCardGame` component utilizing `IonCard` and `IonImg` from Ionic React for visual representation of color cards.
    - Integrated `react-dnd` for drag-and-drop capabilities:
      - Configured `useDrag` to initiate dragging behavior with card details like id, name, and sound.
      - Set up `useDrop` to handle dropping events, allowing interaction between cards.
      - Applied conditional styling to reduce the card's opacity during drag operations, enhancing user feedback.
    - Imported and used `ColorCardGameProps` from `../../../shared/types` to ensure type safety and proper integration of card data.
    - Adjusted CSS path to `../main/GameBoard.css` to apply specific styles for the game board environment.
    - Styled the card with a fixed aspect ratio and full height and width to maintain consistency across devices.
    
    This component enriches the interactive gaming experience by enabling users to drag and drop color cards within the game, promoting engagement and intuitive gameplay.

---
+++ src/components/game/color/ColorCardGame.tsx
@@ -0,0 +1,42 @@
+import { IonCard, IonCardContent, IonImg } from '@ionic/react';
+import React from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+import '../main/GameBoard.css';
+
+import { ColorCardGameProps } from '../../../shared/types';
+
+const ColorCardGame: React.FC<ColorCardGameProps> = ({ card, onDrop }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'CARD',
+    item: { id: card.id, name: card.name, sound: card.sound },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const [, drop] = useDrop({
+    accept: 'CARD',
+    drop: (item: { id: string; name: string; sound: string }) => onDrop(item, card),
+  });
+
+  return (
+    <IonCard
+      className='color-card color-game-card m-0 aspect-square'
+      ref={(node) => drag(drop(node))}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        backgroundColor: '#000000',
+        width: '100%',
+        height: '100%',
+        cursor: 'grab',
+      }}
+    >
+      <IonCardContent className='h-full w-full object-contain'>
+        <IonImg src={card.img} alt={`${card.name} bottle`} className='face' />
+      </IonCardContent>
+    </IonCard>
+  );
+};
+
+export default ColorCardGame;
