a331cbd2e751eb3d1b7fe4a09a2f79f5ee5aa8a1
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-05 02:00:05 +0300

    feat: Implement custom TouchTransition for enhanced drag-and-drop handling
    - Developed a custom `TouchTransition` using `createTransition` to optimize touch interactions in the `HTML5toTouch` drag-and-drop backend configuration:
      - The transition checks for touch events, specifically ensuring `touchEvent.touches` is not null, which indicates a valid touch interaction.
    - Updated the `HTML5toTouch` configuration within `dndConfig.ts` to integrate the new custom transition:
      - Replaced the standard `TouchTransition` from `react-dnd-multi-backend` with the newly created custom transition for the touch backend.
      - Configured the `TouchBackend` with additional options (`enableMouseEvents: true, delayTouchStart: 300`) to improve responsiveness and usability on touch devices.
      - Enabled a preview feature and set the custom transition to effectively manage switching between mouse and touch inputs.
    
    This enhancement allows for more refined control over how touch inputs trigger backend switches, ensuring a smoother and more responsive user experience in applications requiring complex drag-and-drop functionalities.

--- src/dndConfig.ts
+++ src/dndConfig.ts
@@ -1,8 +1,14 @@
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { MouseTransition, TouchTransition } from "react-dnd-multi-backend";
+import { MouseTransition, createTransition, MultiBackendOptions } from "react-dnd-multi-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 
-export const HTML5toTouch = {
+
+const TouchTransition = createTransition('touchstart', (event) => {
+    const touchEvent = event as TouchEvent;
+    return touchEvent.touches != null;
+})
+
+export const HTML5toTouch: MultiBackendOptions = {
     backends: [
         {
             id: 'html5',
@@ -12,7 +18,7 @@ export const HTML5toTouch = {
         {
             id: 'touch',
             backend: TouchBackend,
-           options: { enableMouseEvents: true, delayTouchStart: 500 },
+            options: { enableMouseEvents: true, delayTouchStart: 300 },
             preview: true,
             transition: TouchTransition,
         },
