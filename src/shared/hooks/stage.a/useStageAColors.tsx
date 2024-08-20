13e60479bc93006464c2dc168a2efdb31e3adc9b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-20 18:24:38 +0300

    feat: Implement `useStageAColors` custom hook to fetch and manage color data
    - Created a `useStageAColors` custom hook to handle the fetching and state management of color data for Stage A.
    - Utilized `useState` to manage loading state (`isColorsLoading`) and the fetched colors data.
    - Integrated `CapacitorHttp.get` to perform an HTTP GET request to fetch colors data from the backend API.
    - Implemented error handling to manage potential issues with the data format and network errors.
    - Used `useIonViewWillEnter` to trigger the data fetching when the view is about to enter, ensuring the colors are loaded on view entry.
    
    This hook centralizes the logic for fetching and managing Stage A color data, simplifying component code and improving maintainability.

---
+++ src/shared/hooks/stage.a/useStageAColors.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+import { useIonViewWillEnter } from '@ionic/react';
+
+import { API } from '../../constants';
+import { Color } from '../../types';
+
+export const useStageAColors = () => {
+  const [isColorsLoading, setIsColorsLoading] = useState<boolean>(false);
+  const [colors, setColors] = useState<Color[]>([]);
+
+  const getColors = async () => {
+    setIsColorsLoading(true);
+
+    try {
+      const res: HttpResponse = await CapacitorHttp.get({
+        url: `${API}/stages/stagea/colors`,
+      });
+
+      const data = (await res.data) || {};
+      console.log('data: ', data);
+
+      if (!data || !data[0].stage_a) {
+        throw new Error('Invalid response format');
+      }
+
+      return data[0].stage_a;
+    } catch (error) {
+      console.error('Fetch error: ', error);
+    } finally {
+      setIsColorsLoading(false);
+    }
+  };
+
+  useIonViewWillEnter(() => {
+    const loadColors = async () => {
+      const { colors } = await getColors();
+      setColors(colors);
+      setIsColorsLoading(false);
+    };
+    loadColors();
+  });
+
+  return { colors, setColors, getColors, isColorsLoading };
+};
