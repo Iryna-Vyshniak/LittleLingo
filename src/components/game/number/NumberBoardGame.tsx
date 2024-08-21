d64976ae762f7daabb313a794c687865ec587735
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-22 00:49:22 +0300

    feat: Implemented NumberBoardGame with drag-and-drop functionality
    - Created NumberBoardGame component with drag-and-drop game logic.
    - Added useStageANumbers hook to fetch numbers data for the game.
    - Implemented DraggableNumberSound component for dragging sound buttons.
    - Added NumberCard component to represent each number on the board.
    - Integrated audio feedback (Yippee and Nope) for correct and incorrect actions.
    - Included RefreshButton to regenerate random order of number cards and sounds.

---
+++ src/components/game/number/NumberBoardGame.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+
+import Refresh from '../../../assets/images/refresh.webp';
+import Nope from '../../../assets/sounds/nope.mp3';
+import Yippee from '../../../assets/sounds/yippee.mp3';
+import { useStageANumbers } from '../../../shared/hooks/stage.a/useStageANumbers';
+import { Number } from '../../../shared/types';
+import RefreshButton from '../../common/RefreshButton';
+import DraggableNumberSound from './DraggableNumberSound';
+import NumberCard from './NumberCard';
+
+const NumberBoardGame: React.FC = () => {
+  const [numbersCards, setNumbersCards] = useState<Number[] | []>([]);
+  const [droppedSounds, setDroppedSounds] = useState<{ [key: string]: Number }>(
+    {}
+  );
+  const [soundsButtons, setSoundsButtons] = useState<Number[] | []>([]);
+  const [isActiveRefresh, setIsActiveRefresh] = useState<boolean>(false);
+  const { numbers } = useStageANumbers();
+
+  //this function start new Game
+  function generateCards() {
+    setIsActiveRefresh(true);
+    const randomOrderCards = [...numbers].sort(() => Math.random() - 0.5);
+    const randomOrderSounds = [...numbers].sort(() => Math.random() - 0.5);
+
+    setNumbersCards(randomOrderCards);
+    setSoundsButtons(randomOrderSounds);
+    setTimeout(() => setIsActiveRefresh(false), 1000);
+  }
+
+  useEffect(() => {
+    generateCards();
+  }, [numbers]);
+
+  const handleDrop = (item: Number, correctNumber: string) => {
+    if (item.number === correctNumber) {
+      setDroppedSounds((prevSound) => ({
+        ...prevSound,
+        [correctNumber]: item,
+      }));
+      setSoundsButtons((prevSounds) =>
+        prevSounds.filter((sound) => sound.number !== correctNumber)
+      );
+      const audio = new Audio(Yippee);
+      audio.play();
+    } else {
+      const audio = new Audio(Nope);
+      audio.play();
+    }
+  };
+
+  return (
+    <>
+      <section className='mb-4 flex w-full flex-1 items-center justify-center'>
+        <ul className='flex flex-wrap items-center justify-center gap-4'>
+          {numbersCards.map((item: Number) => (
+            <NumberCard
+              key={item.number}
+              item={item}
+              droppedItem={droppedSounds[item.number]}
+              onDrop={(droppedItem) => handleDrop(droppedItem, item.number)}
+            />
+          ))}
+        </ul>
+      </section>
+      <section className='mb-4 flex w-full flex-1 items-center justify-center'>
+        {' '}
+        <ul className='flex flex-wrap items-center justify-center gap-2'>
+          {soundsButtons.map((item: Number) => (
+            <DraggableNumberSound key={item.sound} item={item} />
+          ))}
+        </ul>
+      </section>
+
+      <section className='flex w-full flex-1 items-center justify-center'>
+        <RefreshButton
+          onClick={generateCards}
+          buttonType='circle'
+          isActive={isActiveRefresh}
+          imgSrc={Refresh}
+        />
+      </section>
+    </>
+  );
+};
+
+export default NumberBoardGame;
