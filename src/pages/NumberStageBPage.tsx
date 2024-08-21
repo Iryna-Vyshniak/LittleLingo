8e6c78439ece8a866b363274ba1eb3f8f1a3a0d9
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-08-22 00:51:38 +0300

    feat: Added NumberStageBPage with drag-and-drop number matching game
    - Created `NumberStageBPage` component, utilizing `DndProvider` for drag-and-drop functionality.
    - Integrated `NumberBoardGame` component for the interactive number matching game.
    - Configured `IonPage`, `IonHeader`, and `IonContent` to structure the page.
    - Added back button navigation using `IonBackButton` with custom styles.
    - Set up the drag-and-drop environment using `react-dnd-multi-backend` with `HTML5toTouch` options.

---
+++ src/pages/NumberStageBPage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonToolbar,
+} from '@ionic/react';
+import { caretBack } from 'ionicons/icons';
+import { DndProvider } from 'react-dnd-multi-backend';
+
+import Title from '../components/common/Title';
+import NumberBoardGame from '../components/game/number/NumberBoardGame';
+import { HTML5toTouch } from '../dndConfig';
+
+const NumberStageBPage: React.FC = () => {
+  return (
+    <DndProvider options={HTML5toTouch}>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar color='primary'>
+            <IonButtons slot='start'>
+              <IonBackButton
+                text='Previous'
+                icon={caretBack}
+                mode='md'
+                defaultHref='/app/list/stageb'
+                className='special-font tracking-wide drop-shadow-[2px_5px_2px_rgba(15,41,1,1)]'
+              ></IonBackButton>
+            </IonButtons>
+            <Title
+              title='Numbers Game'
+              styleType='toolbar'
+              fontSize='text-2xl'
+            />
+          </IonToolbar>
+        </IonHeader>
+        <IonContent
+          fullscreen
+          scrollY={false}
+          className='ion-padding colors-game-bg relative flex flex-col items-center justify-center'
+        >
+          <NumberBoardGame />
+        </IonContent>
+      </IonPage>
+    </DndProvider>
+  );
+};
+
+export default NumberStageBPage;
