43c6b14d37ee056800043bc2f22e080c331597a1
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-13 01:18:21 +0300

    feat: Implemented modal for viewing game rules
    - Added a modal to display game rules on the `NumSecondLevelPage`.
    - Implemented a "View Rules" button that triggers the modal.
    - Modal includes a header with a close button and presents the `Rules` component.
    - Integrated `IonModal`, `IonButton`, and `IonToolbar` for modal functionality and styling.
    - Improved user experience with easy access to game rules within the gameplay screen.

--- src/pages/second.level/NumSecondLevelPage.tsx
+++ src/pages/second.level/NumSecondLevelPage.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonModal,
   IonPage,
   IonToolbar,
 } from '@ionic/react';
@@ -12,6 +14,7 @@ import { caretBack } from 'ionicons/icons';
 import { DndProvider } from 'react-dnd-multi-backend';
 
 import Loader from '../../components/common/Loader';
+import Rules from '../../components/common/Rules';
 import SkeletonList from '../../components/common/SkeletonList';
 import Title from '../../components/common/Title';
 import NumberBoardGame from '../../components/game/2level/number/NumberBoardGame';
@@ -20,9 +23,31 @@ import { useStageANumbers } from '../../shared/hooks/stage.a/useStageANumbers';
 
 const NumSecondLevelPage: React.FC = () => {
   const { numbers, isNumbersLoading } = useStageANumbers();
+  const modal = useRef<HTMLIonModalElement>(null);
+  const page = useRef(null);
+
+  const [presentingElement, setPresentingElement] =
+    useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+
+  function openModal() {
+    modal.current?.present();
+  }
+
+  async function canDismiss(role?: string) {
+    return role !== 'gesture';
+  }
+
   return (
     <DndProvider options={HTML5toTouch}>
-      <IonPage>
+      <IonPage ref={page}>
         <IonHeader>
           <IonToolbar color='primary'>
             <IonButtons slot='start'>
@@ -52,8 +77,42 @@ const NumSecondLevelPage: React.FC = () => {
             </>
           )}
           {!isNumbersLoading && numbers.length && (
-            <NumberBoardGame numbers={numbers} />
+            <>
+              <IonButton expand='block' onClick={openModal}>
+                <span className='special-font custom text-center tracking-wide'>
+                  View Rules
+                </span>
+              </IonButton>
+              <NumberBoardGame numbers={numbers} />
+            </>
           )}
+          <IonModal
+            ref={modal}
+            canDismiss={canDismiss}
+            presentingElement={presentingElement!}
+            className='modal-rules'
+          >
+            <IonHeader>
+              <IonToolbar>
+                <Title
+                  title='Rules'
+                  styleType='modal-title'
+                  fontSize='text-xl'
+                />
+                <IonButtons slot='end'>
+                  <IonButton
+                    onClick={dismiss}
+                    className='special-font custom text-center tracking-wide'
+                  >
+                    <span className='text-white'>Close</span>
+                  </IonButton>
+                </IonButtons>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent>
+              <Rules />
+            </IonContent>
+          </IonModal>
         </IonContent>
       </IonPage>
     </DndProvider>
