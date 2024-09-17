3faad0f990a500f400c3c402d6bf897d4d0dcd27
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-18 00:32:05 +0300

    feat(utils): Added getRandomLetters and getWordWithBlanks functions
    - Implemented getRandomLetters function to generate a mix of word letters and random alphabet letters, ensuring a total length of otherLettersCount.
    - Created getWordWithBlanks function to randomly select two distinct positions in the word and replace the letters with blanks ('_').
    - Ensured that the indexes for blanks are unique.

--- src/shared/utils/index.tsx
+++ src/shared/utils/index.tsx
@@ -66,3 +66,34 @@ export const getRandomOptions = (
   // Shuffle and return the options
   return options.sort(() => Math.random() - 0.5);
 };
+
+// function to generate a mix of word letters and random alphabet letters, ensuring a total length of otherLettersCount
+export const getRandomLetters = (word: string, otherLettersCount = 24) => {
+  const letters = word.split('');
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const otherLetters = Array.from(
+    { length: otherLettersCount - letters.length },
+    () => alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+  );
+  return [...letters, ...otherLetters].sort(() => 0.5 - Math.random());
+};
+
+// function to randomly select two distinct positions in the word and replace the letters with blanks ('_')
+export const getWordWithBlanks = (word: string) => {
+  const letters = word.split('');
+
+  const randomIndex1 = Math.floor(Math.random() * letters.length);
+  let randomIndex2 = Math.floor(Math.random() * letters.length);
+
+  // Check that indexes are not duplicated
+  while (randomIndex2 === randomIndex1) {
+    randomIndex2 = Math.floor(Math.random() * letters.length);
+  }
+
+  const randomIndexes = [randomIndex1, randomIndex2].sort((a, b) => a - b);
+
+  letters[randomIndexes[0]] = '_';
+  letters[randomIndexes[1]] = '_';
+
+  return { wordWithBlanks: letters, missingIndexes: randomIndexes };
+};
