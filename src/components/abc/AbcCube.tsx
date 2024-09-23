e3ae03882d4e7e31e5978ea704824c1f9d7b989a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-24 02:16:14 +0300

    refactor(AbcCube): Improved slide content and interaction logic with hero card animation
    - Updated `AbcCube` to use the new `AnimatedItem<Letter>` type for handling animation classes.
    - Refined slide content creation to include transcription, description, and additional images in the carousel.
    - Replaced manual audio handling with `handleCardClick` function for consistent behavior across components.
    - Enhanced slide layout by adding six distinct slides (capital letter, small letter, transcription, description image, and description text) following a specific pattern.
    - Retained `Swiper` functionality for seamless cube effect and slide transitions while handling clicks to play audio and move to the next slide.

--- src/components/abc/AbcCube.tsx
+++ src/components/abc/AbcCube.tsx
@@ -1,70 +1,100 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import { IonImg } from '@ionic/react';
 import { EffectCube } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper as SwiperCore } from 'swiper/types';
 
-import { useAudioPlayer } from '../../shared/hooks/audio/useAudioPlayer';
-import { ItemProps, Letter } from '../../shared/types';
+import { AnimatedItem, ItemProps, Letter } from '../../shared/types';
+import Title from '../common/Title';
 
-const AbcCube: React.FC<ItemProps<Letter>> = ({ item }) => {
-  const { playAudio, stopAudio } = useAudioPlayer(false);
+const AbcCube: React.FC<ItemProps<AnimatedItem<Letter>>> = ({
+  item,
+  handleCardClick,
+}) => {
   const swiperRef = useRef<SwiperCore | null>(null);
 
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
-
   const handlePlayAudio = () => {
-    if (isPlaying) {
-      stopAudio();
-    } else {
-      playAudio(item.sound);
-    }
-    setIsPlaying(!isPlaying);
     // Move to the next slide
     if (swiperRef.current) {
       swiperRef.current!.slideNext();
+    }
+
+    if (handleCardClick) {
+      handleCardClick(item);
     }
   };
 
   // Create an array with slides following the specified pattern
   const slides = Array.from({ length: 12 }).map((_, index) => {
-    // Determine the content based on the index
     let content;
-    if (index % 3 === 0) {
-      // First slide in each set shows both images
-      content = (
-        <>
+
+    switch (index % 6) {
+      case 0:
+        // First slide — uppercase and lowercase letters
+        content = (
+          <>
+            <IonImg
+              src={item.imageCapitalLetter}
+              alt={item.label}
+              className='h-1/3 w-1/3'
+            />
+            <IonImg
+              src={item.imageSmallLetter}
+              alt={item.label}
+              className='absolute left-[65%] top-[60%] h-[33%] w-[33%] sm:left-[60%] sm:h-1/3 sm:w-1/3'
+            />
+          </>
+        );
+        break;
+      case 1:
+        // The second slide is capital letter only
+        content = (
           <IonImg
             src={item.imageCapitalLetter}
             alt={item.label}
             className='h-1/2 w-1/2'
           />
+        );
+        break;
+      case 2:
+        // The third slide is just a small letter
+        content = (
           <IonImg
             src={item.imageSmallLetter}
             alt={item.label}
-            className='absolute left-[65%] top-[60%] h-[60%] w-[60%] sm:left-[60%] sm:h-1/2 sm:w-1/2'
+            className='h-1/2 w-1/2'
           />
-        </>
-      );
-    } else if (index % 3 === 1) {
-      // Second slide in each set shows only the capital letter
-      content = (
-        <IonImg
-          src={item.imageCapitalLetter}
-          alt={item.label}
-          className='h-1/2 w-1/2'
-        />
-      );
-    } else {
-      // Third slide in each set shows only the small letter
-      content = (
-        <IonImg
-          src={item.imageSmallLetter}
-          alt={item.label}
-          className='h-1/2 w-1/2'
-        />
-      );
+        );
+        break;
+      case 3:
+        // The fourth slide is a transcription
+        content = (
+          <Title
+            title={item.transcription}
+            styleType='card-title'
+            fontSize='text-lg'
+          />
+        );
+        break;
+      case 4:
+        // The fifth slide is a picture of the description
+        content = (
+          <IonImg
+            src={item.imageUrl}
+            alt={item.description}
+            className='h-full w-full object-contain'
+          />
+        );
+        break;
+      case 5:
+        // The sixth slide is the description text
+        content = (
+          <Title title={item.description} styleType='cube-description' />
+        );
+        break;
+      default:
+        break;
     }
 
     return (
@@ -77,7 +107,9 @@ const AbcCube: React.FC<ItemProps<Letter>> = ({ item }) => {
   });
 
   return (
-    <li className='aspect-square flex items-center justify-center'>
+    <li
+      className={`aspect-square flex items-center justify-center ${item.animationClass} p-2`}
+    >
       <Swiper
         effect={'cube'}
         loop={true}
