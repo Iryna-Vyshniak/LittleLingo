be23dde3cf025c416013dea9a6acea9d1566c693
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-24 02:10:45 +0300

    refactor: Moved repeated styles to Tailwind @apply and simplify component logic
    - Refactored repeated styles into Tailwind's `@apply` directive in `global.css`
    - Created reusable utility classes for gradients, shadows, and layout
    - Updated `Title` component to use these new utility classes instead of inline styles
    - Improved maintainability and reduced code duplication by centralizing styles in Tailwind configuration

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -12,13 +12,17 @@ const Title: React.FC<TitleProps> = ({
       case 'app':
         return 'absolute top-0 left-1/2 transform -translate-x-1/2 z-20 p-4 bg-gradient-custom bg-clip-text text-transparent drop-shadow-[2px_5px_2px_rgba(10,28,0,1)]';
       case 'toolbar':
-        return 'flex justify-center items-center self-center p-2 bg-gradient-toolbar bg-clip-text text-transparent drop-shadow-[2px_5px_2px_rgba(15,41,1,1)] text-2xl';
+        return `base-style bg-gradient-toolbar bg-clip-text text-transparent drop-shadow-[2px_5px_2px_rgba(15,41,1,1)] text-2xl`;
       case 'intro':
-        return 'flex items-center justify-center flex-col gap-2 p-4 bg-gradient-custom bg-clip-text text-transparent drop-shadow-[1px_2px_2px_rgba(15,41,1,1)] text-4xl';
+        return `base-style flex-col gap-2 p-4 bg-gradient-custom bg-clip-text text-transparent drop-shadow-[1px_2px_2px_rgba(15,41,1,1)] text-4xl`;
       case 'menu':
         return 'absolute top-0 left-1/2 transform -translate-x-1/2 z-20 p-2 bg-gradient-custom bg-clip-text text-transparent drop-shadow-[2px_5px_2px_rgba(15,41,1,1)] text-3xl';
       case 'card-title':
-        return 'flex justify-center items-center self-center p-2 bg-gradient-custom bg-clip-text text-transparent drop-shadow-[0.5px_1px_1px_rgba(15,41,1,1)] text-xl tracking-wide';
+        return `base-style bg-gradient-custom bg-clip-text text-transparent text-shadow-base`;
+      case 'card-description':
+        return `base-style text-[var(--ion-color-secondary)] text-shadow-base`;
+      case 'cube-description':
+        return `base-style text-[var(--ion-color-secondary)] text-shadow-base`;
       case 'modal-title':
         return 'flex items-center justify-center flex-col gap-2 p-4 text-orange-500 drop-shadow-[0px_0.5px_0.5px_rgba(0,0,1,1)] text-xl uppercase';
       default:
@@ -43,7 +47,15 @@ const Title: React.FC<TitleProps> = ({
         </>
       ) : (
         <p
-          className={`${fontSize} ${fontFamily ? 'gentium-font font-bold' : 'special-font'} custom text-center tracking-wide`}
+          className={`${fontSize === 'text-lg' ? 'xs:text-sm x:text-xl s:text-sm sm:text-lg md:text-2xl lg:text-3xl xl:text-2xl' : fontSize} ${fontFamily ? 'gentium-font font-bold' : 'special-font'} ${
+            styleType === 'card-description'
+              ? 'first-letter:font-bold first-letter:text-red-700'
+              : ''
+          } ${
+            styleType === 'cube-description'
+              ? 'first-letter:font-bold first-letter:text-red-700 xs:text-sm x:text-xl s:text-sm sm:text-lg md:text-2xl lg:text-3xl xl:text-2xl'
+              : ''
+          } custom text-center tracking-wide`}
         >
           {title}
           {subtitle && <span className='ml-2'>{subtitle}</span>}
