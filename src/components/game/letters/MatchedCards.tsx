7df406f68f7ba0f8767ccdaeaa1790a9e99ab952
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-25 23:33:52 +0300

    feat(MatchedCards): Added matched cards display component with audio playback
    - Created `MatchedCards` component to display matched letter cards in a responsive grid
    - Integrated `useAudioPlayer` to play audio when a matched card is clicked
    - Sorted matched cards alphabetically and ensured proper layout using `IonCard` and `IonThumbnail`
    - Added support for displaying both uppercase and lowercase letters in the card title
    - Applied responsive design using Tailwind CSS classes to adapt card size for different screen sizes

---
+++ src/components/game/letters/MatchedCards.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonImg,
+  IonThumbnail,
+} from '@ionic/react';
+
+import { useAudioPlayer } from '../../../shared/hooks/audio/useAudioPlayer';
+import { Letter } from '../../../shared/types';
+
+const MatchedCards: React.FC<{ matchedCards: Letter[] }> = ({
+  matchedCards,
+}) => {
+  const { playAudio } = useAudioPlayer();
+  return (
+    <section className='mb-12'>
+      {' '}
+      <ul className='flex w-full flex-wrap items-center justify-center gap-2'>
+        {matchedCards
+          .filter((card) => card && card.label)
+          .sort((a, b) => a.label.localeCompare(b.label))
+          .map((card) => (
+            <li
+              key={card._id}
+              className='letter-matched-card h-40 w-1/4 lg:w-1/5'
+            >
+              {' '}
+              <IonCard
+                key={card._id}
+                className='ion-no-padding m-0 flex h-full w-full cursor-pointer flex-col items-center justify-center pb-4'
+                onClick={() => playAudio(card.soundDescr)}
+              >
+                <IonThumbnail>
+                  {' '}
+                  <IonImg
+                    src={card.imageUrl}
+                    alt={`${card.label} letter`}
+                    className='h-full w-full object-contain'
+                  />
+                </IonThumbnail>
+
+                <IonCardHeader className='ion-no-padding'>
+                  <IonCardTitle className='base-style gentium-font text-shadow-base text-2xl font-bold text-[var(--ion-color-secondary)]'>
+                    {card.label.toUpperCase()} {card.label.toLowerCase()}
+                  </IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+};
+
+export default MatchedCards;
