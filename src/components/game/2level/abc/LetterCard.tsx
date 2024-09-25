47dc19b54169816426e5377d69db1242e10b8c1b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-25 23:40:23 +0300

    feat(LetterCard): Created LetterCard component.
    - Implemented `useDrag` hook from react-dnd to enable drag-and-drop for letter cards
    - Integrated audio playback when a letter card is clicked using `useAudioPlayer`
    - Added dynamic styles to highlight the card when it's being dragged (e.g., opacity and shadow)
    - Displayed both uppercase and lowercase versions of the letter using the `Title` component
    - Ensured responsive design by adjusting card size based on screen width with Tailwind CSS classes

---
+++ src/components/game/2level/abc/LetterCard.tsx
@@ -0,0 +1,48 @@
+import { IonCard } from '@ionic/react';
+import { useDrag } from 'react-dnd';
+
+import { CardType } from '../../../../shared/constants';
+import { useAudioPlayer } from '../../../../shared/hooks/audio/useAudioPlayer';
+import { Letter } from '../../../../shared/types';
+import Title from '../../../common/Title';
+
+const LetterCard: React.FC<{ letter: Letter }> = ({ letter }) => {
+  const { playAudio } = useAudioPlayer();
+  const [{ isDragging }, dragRef] = useDrag({
+    type: CardType.LETTER,
+    item: { label: letter.label },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <li
+      ref={dragRef}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        cursor: 'grab',
+        boxShadow: isDragging ? '0px 0px 20px yellow' : 'none',
+      }}
+      className='letter-card w-1/3 p-2 xl:w-1/4'
+    >
+      <IonCard
+        className='m-0 flex h-14 items-center justify-center gap-1'
+        onClick={() => playAudio(letter.sound)}
+      >
+        <Title
+          title={letter.label.toUpperCase()}
+          styleType='card-title'
+          fontSize='text-4xl'
+        />
+        <Title
+          title={letter.label.toLowerCase()}
+          styleType='card-title'
+          fontSize='text-4xl'
+        />
+      </IonCard>
+    </li>
+  );
+};
+
+export default LetterCard;
