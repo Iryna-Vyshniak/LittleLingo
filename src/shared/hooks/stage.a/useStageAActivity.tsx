176e22fcb4feacc70c333691687658797a227674
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-09-29 16:01:36 +0300

    feat: Added useStageAactivity hook for managing activity data with Zustand
    - Integrated Zustand store in `useStageAactivity` to manage `activity` and `isActivityLoading` states
    - Added `getActivity` function to fetch activity data from the API
    - Refactored state handling to use Zustand store for setting and retrieving activity data
    - Implemented `useIonViewWillEnter` to trigger data loading when the view is entered

---
+++ src/shared/hooks/stage.a/useStageAActivity.tsx
@@ -0,0 +1,54 @@
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+import { useIonViewWillEnter } from '@ionic/react';
+
+import { API } from '../../constants';
+import { useStageStore } from '../../store/useStageStore';
+
+export const useStageAactivity = () => {
+  const isActivityLoading = useStageStore((store) => store.isActivityLoading);
+  const setIsActivityLoading = useStageStore(
+    (store) => store.setIsActivityLoading
+  );
+  const activity = useStageStore((store) => store.activity);
+  const setActivity = useStageStore((store) => store.setActivity);
+
+  const getActivity = async () => {
+    if (activity.length > 0) {
+      return activity;
+    }
+
+    setIsActivityLoading(true);
+
+    try {
+      const res: HttpResponse = await CapacitorHttp.get({
+        url: `${API}/stages/stagea/activity`,
+      });
+
+      const data = (await res.data) || {};
+
+      if (!data || !data[0].stage_a) {
+        throw new Error('Invalid response format');
+      }
+      console.log(data[0]);
+      return data[0].stage_a;
+    } catch (error) {
+      console.error('Fetch error: ', error);
+    } finally {
+      setIsActivityLoading(false);
+    }
+  };
+
+  useIonViewWillEnter(() => {
+    const loadActivity = async () => {
+      if (!activity.length) {
+        setIsActivityLoading(true);
+        const { activity } = await getActivity();
+        setActivity(activity);
+        setIsActivityLoading(false);
+      }
+    };
+    loadActivity();
+  });
+
+  return { activity, setActivity, getActivity, isActivityLoading };
+};
