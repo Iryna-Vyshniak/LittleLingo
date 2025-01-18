999187100eb0bf6301d856304e71f93586609bc5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-01-18 21:31:49 +0200

    refactor: streamline NumberItem component and enhance styling for improved layout and responsiveness

--- src/components/numbers/NumberItem.tsx
+++ src/components/numbers/NumberItem.tsx
@@ -1,47 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { IonImg, IonPopover } from '@ionic/react';
+import { IonImg, IonPopover, IonThumbnail } from '@ionic/react';
 
-import NumberDragon from '../../assets/images/dragon-game.png';
 import { useAudioPlayer } from '../../shared/hooks/audio/useAudioPlayer';
 import { ItemProps, Number } from '../../shared/types';
 import { generateGemImages } from '../../shared/utils';
 
 const NumberItem: React.FC<ItemProps<Number>> = ({ item }) => {
   const { playAudio } = useAudioPlayer(true);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [hasError, setHasError] = useState<boolean>(false);
+
+  const dragonImageUrl = item.imageUrl || '';
   return (
     <li className='flex items-center justify-center'>
       <button
         id={`hover-trigger-${item._id}`}
         onClick={() => playAudio(item.sound)}
-        className='number-btn special-font custom text-center tracking-wide'
+        className='number-btn special-font custom relative flex h-[12vmin] w-32 flex-col items-center justify-end gap-2 text-center tracking-wide md:h-[8vmin] md:w-[10vmin] md:items-center lg:w-[12vmin]'
         data-label={item.number}
       >
-        <span className='number-img-wrapper'>
-          {isLoading && !hasError && (
+        {dragonImageUrl ? (
+          <IonThumbnail className='number-img-wrapper absolute -top-6 left-[60%] z-20 w-1/3 -translate-x-[60%] transform md:-top-2 md:w-1/2'>
             <IonImg
-              src={NumberDragon} /* Default image */
-              alt='default dragon'
-              className='number-img'
+              src={item.imageUrl || ''}
+              alt={item.label + 'dragon baby'}
+              className='number-img h-auto w-full'
             />
-          )}
+          </IonThumbnail>
+        ) : null}
 
-          <IonImg
-            src={item.imageUrl || ''}
-            alt={item.label + 'dragon baby'}
-            className='number-img'
-            onIonImgWillLoad={() => setIsLoading(true)}
-            onIonImgDidLoad={() => setIsLoading(false)} // Image loaded
-            onIonError={() => {
-              setIsLoading(false);
-              setHasError(true); // If loading error
-            }}
-          />
-        </span>
-
-        <span className='number-label p-4'>{item.label}</span>
+        <span className='number-label mx-4 pl-6'>{item.label}</span>
       </button>
       <IonPopover
         trigger={`hover-trigger-${item._id}`}
