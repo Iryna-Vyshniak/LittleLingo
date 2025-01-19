07367834ed7bf0fd14d9496ebbacd38c81807031
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2025-01-19 23:25:43 +0200

    refactor: enhance DropNumberZone component to display item label and update NumberCard integration for improved clarity

--- src/components/game/number/DropNumberZone.tsx
+++ src/components/game/number/DropNumberZone.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { IonLabel, IonThumbnail } from '@ionic/react';
 import { useDrop } from 'react-dnd';
 
 import DragHere from '../../../assets/images/drag-here.png';
@@ -7,11 +8,16 @@ import { CardType } from '../../../shared/constants';
 import { Number } from '../../../shared/types';
 
 interface DropZoneProps {
+  item: Number;
   onDrop: (item: Number) => void;
   droppedItem?: Number;
 }
 
-const DropNumberZone: React.FC<DropZoneProps> = ({ onDrop, droppedItem }) => {
+const DropNumberZone: React.FC<DropZoneProps> = ({
+  onDrop,
+  droppedItem,
+  item,
+}) => {
   const [{ isOver }, drop] = useDrop({
     accept: CardType.SOUND,
     drop: (item: { item: Number }) => {
@@ -30,18 +36,24 @@ const DropNumberZone: React.FC<DropZoneProps> = ({ onDrop, droppedItem }) => {
       }}
       className={`border-2 border-green-700 flex h-full w-full items-center justify-center border-dashed ${droppedItem ? 'drop-place' : '#ffc885'}`}
     >
+      <IonLabel className='special-font custom number-card-caption drop-place flex h-full items-center justify-center px-2 py-1 text-center tracking-wide'>
+        {item.label}
+      </IonLabel>
       {droppedItem ? (
         <p className='special-font custom drop-number px-4 text-center tracking-wide'>
           {droppedItem.number}
         </p>
       ) : (
-        <img
-          src={DragHere}
-          alt='drag here'
-          width={96}
-          height={96}
-          className='h-full w-full object-cover'
-        />
+        <IonThumbnail className='h-full w-10 md:w-[6vmin]' slot='end'>
+          {' '}
+          <img
+            src={DragHere}
+            alt='drag here'
+            width={96}
+            height={96}
+            className='h-full w-full object-contain'
+          />
+        </IonThumbnail>
       )}
     </div>
   );
--- src/components/game/number/NumberCard.tsx
+++ src/components/game/number/NumberCard.tsx
@@ -10,10 +10,7 @@ const NumberCard: React.FC<{
 }> = ({ item, onDrop, droppedItem }) => {
   return (
     <li className='number-card flex h-[6vmin] items-center justify-center'>
-      <p className='special-font custom number-card-caption flex items-center justify-center p-4 text-center tracking-wide'>
-        {item.label}
-      </p>
-      <DropNumberZone onDrop={onDrop} droppedItem={droppedItem} />
+      <DropNumberZone onDrop={onDrop} droppedItem={droppedItem} item={item} />
     </li>
   );
 };
